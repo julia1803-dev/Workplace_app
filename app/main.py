2e2ae13e3118a5d93c8ba9990c157ea5d87c407f
u236358 <yuliya.krause2@sbb.ch>
2026-04-10 13:17:57 +0200

    fix gitignore and seed data

--- app/main.py
+++ app/main.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI
-from app.database import create_db
+from sqlmodel import Session, select
+from app.database import create_db, engine
 from app.routers import router
+from app.models import Team
 
 app = FastAPI(title="Workplace Booking App")
+
+
+def seed_data():
+    with Session(engine) as session:
+        existing_team = session.exec(select(Team)).first()
+        if not existing_team:
+            team = Team(name="IT Team")
+            session.add(team)
+            session.commit()
+            print("Seed-Daten erstellt!")
 
 
 @app.on_event("startup")
 def on_startup():
     create_db()
+    seed_data()
 
 
 app.include_router(router)
